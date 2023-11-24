c1fc494bd4fef394baaf5b71e44d53a39f442956
fath-purn <talitakamilia345@gmail.com>
2023-11-24 13:49:26 +0700

    feat: login user

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -2,111 +2,6 @@ const prisma = require("../libs/prisma");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
-const loginAdmin = async (req, res, next) => {
-  try {
-    const { idAdmin, password } = req.body;
-    const admin = await prisma.admin.findUnique({
-      where: {
-        idAdmin: idAdmin,
-      },
-    });
-
-    if (!admin) {
-      return res.status(404).json({
-        success: false,
-        message: "Admin not found",
-        data: null,
-      });
-    }
-
-    const isMatch = await bcrypt.compare(password, admin.password);
-
-    if (!isMatch) {
-      return res.status(400).json({
-        success: false,
-        message: "Wrong id or Password",
-        data: null,
-      });
-    }
-
-    const payload = {
-      id: admin.id,
-      idAdmin: admin.idAdmin,
-    };
-
-    const token = jwt.sign(payload, process.env.JWT_SECRET, {
-      expiresIn: "1d",
-    });
-
-    return res.status(200).json({
-      success: true,
-      message: "Login success",
-      data: token,
-    });
-  } catch (error) {
-    next(error);
-  }
-};
-
-// register
-const register = async (req, res, next) => {
-  try {
-    let { nickname, email, password, password_confirmation } = req.body;
-
-    if (password != password_confirmation) {
-      return res.status(400).json({
-        status: false,
-        message: "Bad Requset",
-        err: "Password & password confirmation do not match!",
-        data: null,
-      });
-    }
-
-    let userExist = await prisma.users.findUnique({
-      where: { email: email },
-    });
-    if (userExist) {
-      return res.status(400).json({
-        status: false,
-        message: "Bad Request",
-        err: "Email already exists!",
-        data: null,
-      });
-    }
-
-    let encryptedPassword = await bcrypt.hash(password, 10);
-
-    let users = await prisma.users.create({
-      data: {
-        nickname,
-        email,
-        password: encryptedPassword,
-      },
-    });
-
-    const user_id = users.id;
-    await prisma.profiles.create({
-      data: {
-        users_id: user_id,
-        first_name: null,
-        last_name: null,
-        profile_picture: null,
-        city: null,
-        country: null,
-      },
-    });
-
-    return res.status(201).json({
-      status: true,
-      message: "Created Succesfuly!",
-      err: null,
-      data: { users },
-    });
-  } catch (err) {
-    next(err);
-  }
-};
-
 // login user
 const loginUser = async (req, res, next) => {
   try {
@@ -170,8 +65,5 @@ const authenticate = (req, res, next) => {
 };
 
 module.exports = {
-  loginAdmin,
   loginUser,
-  register,
-  authenticate,
 };
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,14 +1,10 @@
 const router = require('express').Router();
-const { loginAdmin, register, loginUser, authenticate } = require('../controllers/auth.controller');
-const {restrict} = require('../middlewares/auth.middlewares');
+const { loginUser } = require('../controllers/auth.controller');
 
 router.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-router.post('/admin/login', loginAdmin);
-router.post('/register', register);
 router.post('/login', loginUser);
-router.get('/whoami', restrict, authenticate);
 
 module.exports = router;
