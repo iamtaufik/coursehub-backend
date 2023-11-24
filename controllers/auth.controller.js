848b49bdcc70abf5f9811146d454c8609026857c
jenien <riska.jeen@gmail.com>
2023-11-24 19:36:22 +0700

    add otp

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,6 +1,9 @@
 const prisma = require("../libs/prisma");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = process.env;
+const otpHandler= require("../libs/otpHandler");
+const nodemailer = require("../libs/nodemailer");
 
 // login user
 const loginUser = async (req, res, next) => {
@@ -102,17 +105,26 @@ const register = async (req, res, next) => {
         country: null
       }
     });
+    const token = jwt.sign({ user_id: users.id }, JWT_SECRET, { expiresIn: '1h' });
+    
+    const otp = await otpHandler.generateOTP(email);
+    const html = `Your OTP for account activation is: <strong>${otp}</strong>`;
+    await nodemailer.sendEmail(email, 'Account Activation OTP', html);
+
 
     return res.status(201).json({
       status: true,
-      message: 'Created Succesfuly!',
+      message: 'Created Successfully!',
       err: null,
-      data: { users }
+      data: {
+        users,
+        token, // Menambahkan token ke respons
+      }
     });
   } catch (err) {
     next(err);
   }
-}
+};
 
 const loginAdmin = async (req, res, next) => {
   try {
@@ -170,8 +182,90 @@ const authenticate = (req, res, next) => {
   });
 };
 
+const verifyOTP = async (req, res) => {
+  try {
+      const token = req.headers.authorization;
+
+      if (!token || !token.startsWith('Bearer ')) {
+          return res.status(401).json({
+              status: false,
+              message: 'Token not found or invalid',
+              err: null,
+              data: null
+          });
+      }
+
+      const tokenValue = token.split(' ')[1]; // Mengambil token setelah "Bearer "
+
+      if (!tokenValue) {
+          return res.status(401).json({
+              status: false,
+              message: 'Token not found or invalid',
+              err: null,
+              data: null
+          });
+      }
+
+      const decoded = jwt.verify(tokenValue, process.env.JWT_SECRET);
+
+      if (!decoded.email) {
+          return res.status(400).json({
+              status: false,
+              message: 'Bad request',
+              err: 'Email not found in token',
+              data: null
+          });
+      }
+
+      const email = decoded.email;
+      const { otp } = req.body;
+
+      const user = await prisma.users.findUnique({ where: { email } });
+
+      if (!user) {
+          return res.status(404).json({
+              status: false,
+              message: 'User not found',
+              err: null,
+              data: null
+          });
+      }
+
+      const storedOTP = await otpHandler.getOTPFromStorage(email);
+
+      if (otp !== storedOTP) {
+          return res.status(400).json({
+              status: false,
+              message: 'Bad request',
+              err: 'Invalid or expired OTP',
+              data: null
+          });
+      }
+
+      // OTP yang sesuai, tandai verifikasi pengguna di sini jika perlu
+      await prisma.users.update({
+          where: { email },
+          data: { is_verified: true, otp: null }
+      });
+
+      return res.json({
+          status: true,
+          message: 'Account activated successfully',
+          err: null,
+          data: null
+      });
+  } catch (err) {
+      return res.status(500).json({
+          status: false,
+          message: 'Error activating account',
+          err: err.message,
+          data: null
+      });
+  }
+};
 module.exports = {
   loginUser,
   loginAdmin,
-  register
+  register,
+  verifyOTP
 };
