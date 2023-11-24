1cd1879b8996ed35cd64cad46b205beba1158f01
fath-purn <talitakamilia345@gmail.com>
2023-11-24 13:32:38 +0700

    feat: login user

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,6 +1,7 @@
-const prisma = require('../libs/prisma');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const prisma = require("../libs/prisma");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
 const loginAdmin = async (req, res, next) => {
   try {
     const { idAdmin, password } = req.body;
@@ -13,7 +14,7 @@ const loginAdmin = async (req, res, next) => {
     if (!admin) {
       return res.status(404).json({
         success: false,
-        message: 'Admin not found',
+        message: "Admin not found",
         data: null,
       });
     }
@@ -23,7 +24,7 @@ const loginAdmin = async (req, res, next) => {
     if (!isMatch) {
       return res.status(400).json({
         success: false,
-        message: 'Wrong id or Password',
+        message: "Wrong id or Password",
         data: null,
       });
     }
@@ -34,12 +35,12 @@ const loginAdmin = async (req, res, next) => {
     };
 
     const token = jwt.sign(payload, process.env.JWT_SECRET, {
-      expiresIn: '1d',
+      expiresIn: "1d",
     });
 
     return res.status(200).json({
       success: true,
-      message: 'Login success',
+      message: "Login success",
       data: token,
     });
   } catch (error) {
@@ -47,6 +48,130 @@ const loginAdmin = async (req, res, next) => {
   }
 };
 
+// register
+const register = async (req, res, next) => {
+  try {
+    let { nickname, email, password, password_confirmation } = req.body;
+
+    if (password != password_confirmation) {
+      return res.status(400).json({
+        status: false,
+        message: "Bad Requset",
+        err: "Password & password confirmation do not match!",
+        data: null,
+      });
+    }
+
+    let userExist = await prisma.users.findUnique({
+      where: { email: email },
+    });
+    if (userExist) {
+      return res.status(400).json({
+        status: false,
+        message: "Bad Request",
+        err: "Email already exists!",
+        data: null,
+      });
+    }
+
+    let encryptedPassword = await bcrypt.hash(password, 10);
+
+    let users = await prisma.users.create({
+      data: {
+        nickname,
+        email,
+        password: encryptedPassword,
+      },
+    });
+
+    const user_id = users.id;
+    await prisma.profiles.create({
+      data: {
+        users_id: user_id,
+        first_name: null,
+        last_name: null,
+        profile_picture: null,
+        city: null,
+        country: null,
+      },
+    });
+
+    return res.status(201).json({
+      status: true,
+      message: "Created Succesfuly!",
+      err: null,
+      data: { users },
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+// login user
+const loginUser = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    const user = await prisma.users.findUnique({
+      where: {
+        email: email,
+      },
+    });
+
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "Bad Request",
+        err: "User not found",
+        data: null,
+      });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({
+        success: false,
+        message: "Bad Request",
+        err: "Wrong Password",
+        data: null,
+      });
+    }
+
+    const payload = {
+      id: user.id,
+      email: user.email,
+    };
+
+    const token = jwt.sign(payload, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+
+    return res.status(200).json({
+      success: true,
+      message: "Login success",
+      err: null,
+      data: {
+        user: user,
+        token: token,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const authenticate = (req, res, next) => {
+  return res.status(200).json({
+    status: true,
+    message: "OK",
+    err: null,
+    data: { user: req.user },
+  });
+};
+
 module.exports = {
   loginAdmin,
+  loginUser,
+  register,
+  authenticate,
 };
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,10 +1,14 @@
 const router = require('express').Router();
-const { loginAdmin } = require('../controllers/auth.controller');
+const { loginAdmin, register, loginUser, authenticate } = require('../controllers/auth.controller');
+const {restrict} = require('../middlewares/auth.middlewares');
 
 router.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
 router.post('/admin/login', loginAdmin);
+router.post('/register', register);
+router.post('/login', loginUser);
+router.get('/whoami', restrict, authenticate);
 
 module.exports = router;
