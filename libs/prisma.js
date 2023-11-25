eb873f0957440f0a418c2d89580f223b2355d44f
iamtaufik <muhtopik07@gmail.com>
2023-11-25 20:41:16 +0700

    feat: fix prisma generated

--- libs/prisma.js
+++ libs/prisma.js
@@ -1,14 +1,5 @@
-require('dotenv').config();
 const { PrismaClient } = require('@prisma/client');
 
-var prisma;
-
-if (process.env.NODE_ENV === 'production') {
-  const { PrismaClient } = require('@prisma/client/edge');
-  prisma = new PrismaClient();
-  module.exports = prisma;
-}
-
-prisma = new PrismaClient();
+const prisma = new PrismaClient();
 
 module.exports = prisma;
