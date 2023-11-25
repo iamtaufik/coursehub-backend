b7bc3bda59bf057c335806231b878bf9c0e1cba6
iamtaufik <muhtopik07@gmail.com>
2023-11-25 20:52:37 +0700

    feat: fix cros and documentaion

--- app.js
+++ app.js
@@ -10,7 +10,11 @@ const path = require('path');
 const { serverError, notFound } = require('./middlewares/errorHandling');
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: '*',
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
