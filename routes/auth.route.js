26b18b8409464df3afd872882cb28b786bc372b4
iamtaufik <muhtopik07@gmail.com>
2023-11-27 20:00:13 +0700

    fix: verify-token

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,6 +1,5 @@
 const router = require('express').Router();
-const { loginAdmin, register, loginUser, verifyOTP, authenticate, createAdmin, resetPassword } = require('../controllers/auth.controller');
-const verifyToken = require('../libs/verifyToken');
+const { loginAdmin, register, loginUser, verifyOTP, authenticate, createAdmin } = require('../controllers/auth.controller');
 const verifyToken = require('../middlewares/verifyToken');
 const verifyAdmin = require('../middlewares/verifyAdmin');
 
@@ -14,7 +13,6 @@ router.post('/verifyOTP', verifyOTP);
 router.post('/admin/login', loginAdmin);
 router.post('/admin/register', createAdmin);
 router.get('/whoami', verifyToken, authenticate);
-router.post('/resetPassword', resetPassword);
 router.get('/admin/whoami', verifyToken, verifyAdmin, authenticate);
 
 module.exports = router;
