4454fc4d85875c890819373e47a94232c5ba53ba
fath-purn <talitakamilia345@gmail.com>
2023-11-29 21:17:17 +0700

    feat: delete course

--- routes/course.route.js
+++ routes/course.route.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
-const { createCourse, getCourses} = require('../controllers/course.controller');
+const { createCourse, getCourses, deleteCourse} = require('../controllers/course.controller');
 const verifyToken = require('../middlewares/verifyToken');
 const verifyAdmin = require('../middlewares/verifyAdmin');
 
 router.post('/', verifyToken, verifyAdmin, createCourse);
 router.get('/', getCourses);
+router.delete('/:id', verifyToken, verifyAdmin, deleteCourse);
 
 module.exports = router;
