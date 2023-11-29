f4315f5bd3ceb14864a63680937b10f8ff1e7ccf
DhimasRizaldy <dtekchannel02@gmail.com>
2023-11-29 15:43:11 +0700

    feat/docs-update

---
+++ prisma/migrations/20231129044756_init/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `isActive` on the `Courses` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Courses" DROP COLUMN "isActive";
