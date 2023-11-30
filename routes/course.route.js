4be5f69118ce5856fdd7b98da1015f83dd6abd9e
DhimasRizaldy <dtekchannel02@gmail.com>
2023-11-30 14:51:25 +0700

    feat/detail-course

--- routes/course.route.js
+++ routes/course.route.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
-const { createCourse, getCourses} = require('../controllers/course.controller');
+const { createCourse, getCourses, getDetailCourses } = require('../controllers/course.controller');
 const verifyToken = require('../middlewares/verifyToken');
 const verifyAdmin = require('../middlewares/verifyAdmin');
 
 router.post('/', verifyToken, verifyAdmin, createCourse);
 router.get('/', getCourses);
+router.get('/:id', getDetailCourses);
 
 module.exports = router;
