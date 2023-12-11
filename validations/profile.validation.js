c67bd411e562e5172ce9ed7b3e19fade5d1eda5a
iamtaufik <muhtopik07@gmail.com>
2023-12-11 19:44:17 +0700

    hotfix: profile

--- validations/profile.validation.js
+++ validations/profile.validation.js
@@ -2,8 +2,7 @@ const joi = require('joi');
 
 const createProfileSchema = joi.object({
   phone_number: joi.string().required(),
-  first_name: joi.string().required(),
-  last_name: joi.string().required(),
+  full_name: joi.string().required(),
   city: joi.string().required(),
   country: joi.string().required(),
 });
